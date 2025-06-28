771037ac687e4ba12ceda3295889d399291ea06c
Karsten Zhou <54686209+XiaoSong-CPE@users.noreply.github.com>
2025-06-28 09:14:58 +0800

    chore: update manifest.json fetch URL to raw.githubusercontent

--- public/entry.js
+++ public/entry.js
@@ -1,7 +1,7 @@
 (async () => {
   // 获取vite构建的manifest.json文件
   let response = await fetch(
-    "https://cdn.jsdelivr.net/gh/XYY-huijiwiki/monaco-editor-for-mediawiki@dist/.vite/manifest.json"
+    "https://raw.githubusercontent.com/XYY-huijiwiki/monaco-editor-for-mediawiki/dist/.vite/manifest.json"
   );
   let data = await response.json();
   // 根据manifest.json文件获取入口文件的url
