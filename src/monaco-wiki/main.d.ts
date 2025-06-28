d2207736648302b4caeb6597eb21ec48ad7105a4
Karsten Zhou <54686209+XiaoSong-CPE@users.noreply.github.com>
2025-06-28 12:58:41 +0800

    feat: integrate `monaco-wiki` and brings all feature from it

---
+++ src/monaco-wiki/main.d.ts
@@ -0,0 +1,4 @@
+import type { ConfigData } from "wikiparser-node";
+import type * as Monaco from "monaco-editor";
+declare const _default: (monaco: typeof Monaco, parserConfig?: ConfigData | string | boolean, lang?: string | string[], cdn?: string) => Promise<void>;
+export default _default;
--- src/monaco.ts
+++ src/monaco.ts
@@ -1,20 +1,11 @@
 import loader from "@monaco-editor/loader";
-import * as monaco from "monaco-editor";
-import { shikiToMonaco } from "@shikijs/monaco";
-import { createHighlighter } from "shiki";
 import { setTheme } from "@fluentui/web-components";
 import { webLightTheme, webDarkTheme } from "@fluentui/tokens";
+import registerWiki from "./monaco-wiki/main";
 
 import getLangCode from "./utils/getLangCode";
 
-// Prepare Monaco and Shiki
-const highlighter = await createHighlighter({
-  themes: ["dark-plus", "light-plus"],
-  langs: ["wikitext", "json", "html", "javascript", "css", "lua"],
-});
-
 loader.config({
-  monaco: monaco,
   "vs/nls": {
     availableLanguages: {
       "*": getLangCode(mw.config.get("wgUserLanguage")),
@@ -23,59 +14,18 @@ loader.config({
 });
 
 const monacoInstance = await loader.init();
-["wikitext", "json", "html", "javascript", "css", "lua", "plaintext"].forEach(
-  (lang) => {
-    monacoInstance.languages.register({ id: lang });
-  }
-);
-shikiToMonaco(highlighter, monacoInstance);
 
-// additional wikitext support
-monacoInstance.languages.setLanguageConfiguration("wikitext", {
-  comments: {
-    blockComment: ["<!--", "-->"],
-  },
-  brackets: [
-    ["[", "]"],
-    ["-{", "}-"],
-    ["{|", "|}"],
-    ["{{", "}}"],
-    ["{{{", "}}}"],
-  ],
-  autoClosingPairs: [
-    { open: "{", close: "}" },
-    { open: "[", close: "]" },
-    { open: "-{", close: "}-" },
-    { open: "<!--", close: "-->" },
-    { open: "<b>", close: "</b>" },
-    { open: "<blockquote>", close: "</blockquote>" },
-    { open: "<ce>", close: "</ce>" },
-    { open: "<chem>", close: "</chem>" },
-    { open: "<code>", close: "</code>" },
-    { open: "<div>", close: "</div>" },
-    { open: "<graph>", close: "</graph>" },
-    { open: "<hiero>", close: "</hiero>" },
-    { open: "<i>", close: "</i>" },
-    { open: "<includeonly>", close: "</includeonly>" },
-    { open: "<math>", close: "</math>" },
-    { open: "<noinclude>", close: "</noinclude>" },
-    { open: "<nowiki>", close: "</nowiki>" },
-    { open: "<onlyinclude>", close: "</onlyinclude>" },
-    { open: "<pre>", close: "</pre>" },
-    { open: "<ref>", close: "</ref>" },
-    { open: "<s>", close: "</s>" },
-    { open: "<score>", close: "</score>" },
-    { open: "<span>", close: "</span>" },
-    { open: "<syntaxhighlight>", close: "</syntaxhighlight>" },
-    { open: "<templatedata>", close: "</templatedata>" },
-  ],
-  surroundingPairs: [
-    { open: "{", close: "}" },
-    { open: "[", close: "]" },
-    { open: "(", close: ")" },
-    { open: "'", close: "'" },
-  ],
-});
+await registerWiki(
+  monacoInstance,
+
+  // Set to `true` if used in a MediaWiki site,
+  // or a string to specify a preset configuration (https://github.com/bhsd-harry/wikiparser-node/tree/main/config)
+  true,
+
+  // (optional) i18n language codes with a preferred order,
+  // e.g. `['zh-hans', 'zh-hant', 'en']`
+  [getLangCode(mw.config.get("wgUserLanguage"))]
+);
 
 // Theme management
 const updateTheme = () => {
